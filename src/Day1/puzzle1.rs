59d3be177b5dd1596b7d5bc2264313cfb45b63e0
Akshad135 <akshad.agrawal1978@gmail.com>
2024-12-02 18:42:00 +0530

    first commit

---
+++ src/Day1/puzzle1.rs
@@ -0,0 +1,35 @@
+use std::fs::File;
+use std::io::{self,BufRead};
+
+pub fn solve() -> io::Result<()>{
+    let file_path = "Day1/puzzle1.txt";
+    let file = File::open(file_path)?;
+    let reader = io::BufReader::new(file);
+
+    let mut left: Vec<i32> = Vec::new();
+    let mut right: Vec<i32> = Vec::new();
+
+    for line in reader.lines(){
+        let currentLine = line?;
+        let nums: Vec<i32> = currentLine
+                    .split_whitespace()
+                    .filter_map(|s| s.parse::<i32>().ok())
+                    .collect();
+
+        left.push(nums[0]);
+        right.push(nums[1]);
+    }
+
+    left.sort();
+    right.sort();
+
+    let mut total = 0;
+
+    for i in 0..left.len(){
+        let diff = left[i] - right[i];
+        total += diff.abs();
+    }
+
+    println!("{}",total);
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,9 @@
+mod Day1{
+    pub mod puzzle1;
+}
+
+fn main() -> std::io::Result<()> {
+    let _ = Day1::puzzle1::solve();
+
+    Ok(())
+}
